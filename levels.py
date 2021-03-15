02923c9b5d9488e464e39daafb937d1dd7f2aa0d
williamjamesj <61896069+williamjamesj@users.noreply.github.com>
2021-03-15 20:26:34 +1000

    level one was definitely too hard, and has now been fixed

--- levels.py
+++ levels.py
@@ -10,11 +10,11 @@ def levelone():
     globals.playerspaceship = Spaceship(globals.spawnPointLocation,"yellowspaceship",10,0.1,0,2)
     globals.allobjects.add(globals.playerspaceship)
     globals.screen.blit(globals.backgroundpicture, (0,0)) 
-    globals.allnonplayers.add(Barrier(500,250,50,650)) # Wall 1
-    globals.allnonplayers.add(Barrier(200,100,50,650)) # Wall 2
-    globals.allnonplayers.add(Barrier(-100,250,50,650)) # Wall 3
-    globals.allnonplayers.add(Barrier(-400,100,50,650)) # Wall 4
-    globals.allnonplayers.add(Barrier(-850,700,500,50)) # Wall 5
+    globals.allnonplayers.add(Barrier(500,300,50,600)) # Wall 1
+    globals.allnonplayers.add(Barrier(200,100,50,600)) # Wall 2
+    globals.allnonplayers.add(Barrier(-100,300,50,600)) # Wall 3
+    globals.allnonplayers.add(Barrier(-400,100,50,600)) # Wall 4
+    globals.allnonplayers.add(Barrier(-800,650,400,50)) # Wall 5
     globals.allnonplayers.add(Barrier(-1000,0,2000,100)) # Top
     globals.allnonplayers.add(Barrier(-1100,0,100,1000)) # Left
     globals.allnonplayers.add(Barrier(-1000,900,2000,100)) # Bottom
